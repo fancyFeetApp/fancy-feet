ba7e4e0ec1548f5e42209fa5fbc273893b453b27
Christine <aqui.christine@gmail.com>
2018-03-22 21:53:49 -0400

    setup component file
    component file for game input started.

---
+++ dev/scripts/gameInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GameInput extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Some text</h2>
+			</div>
+		);
+	}
+}
+
+export default GameInput;
