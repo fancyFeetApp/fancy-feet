aab4277e653bd5fe1c3d9ab5ad2e28a525222732
natalieronson <natalie.ronson@gmail.com>
2018-03-28 21:22:33 -0400

    Game info display

---
+++ dev/scripts/gameInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class GameInfo extends React.Component {
+    render() {
+        return (
+            <div>
+            
+                <p>Location</p>
+                <p>Field</p>
+                <p>Num players</p>
+                <p>Date</p>
+                <button>Join</button>
+                <button>Twitter</button>
+                <button>Facebook</button>
+                <button>Edit</button>
+                
+            
+            </div>
+        )
+    }
+}
